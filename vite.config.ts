3c12fb20bf7977b553ec7a9fff171223c91af5f2
Daniel Vicente <danielvicente63@gmail.com>
2025-08-07 23:54:06 -0300

    ajustes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/Portfolio/",
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  return {
+    base: command === "build" ? "/Portfolio/" : "/",
+    plugins: [react()],
+  };
 });
